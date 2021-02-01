eccb491534c4665e3d6f89ce5e172d8c5aa1f810
张晓立 <zhangxiaoli@yungeio.com>
2021-02-01 10:40:45 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//app.js
+App({
+    onLaunch: function () {
+		
+    },
+    /**
+     * 全局数据
+     */
+    globalData: {
+		
+    },
+});
---
+++ env.js
@@ -0,0 +1,45 @@
+/**
+ * 启用的环境
+ * @type {{mode: string, dev: {domain: string}, test: {domain: string}, prod: {domain: string}}}
+ */
+const env = {
+  mode: 'dev', // dev || test || prod
+  // mode: 'prod', // dev || test || prod
+
+  dev: {
+    domain: '',
+  },
+
+  test: {
+    domain: '',
+  },
+
+  prod: {
+  },
+
+  /**
+   * 封装 URL 获取不同环境的 API
+   * @param api
+   * @returns {*}
+   */
+  url: function(api) {
+    let domain = '';
+    switch (env.mode) {
+      case 'test':
+        domain = env.test.domain;
+        break;
+      case 'prod':
+        domain = env.prod.domain;
+        break;
+
+      case 'dev':
+      default:
+        domain = env.dev.domain;
+
+    }
+    return domain + api;
+  }
+
+};
+
+module.exports = env;
